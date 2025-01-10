fb6a4ae8e26c3aa80d2de6d3c5fd0119663c8713
caryslewelling <cmlewelling@cps.edu>
2025-01-10 08:35:14 -0600

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,53 @@
+#Conditional Statements
+
+#Initials
+#Functions
+def vote_check():
+    citizen = input("Are you a US citizen?")
+    age = int(input("How old are you?"))
+    if age >= 18 and citizen == Yes:
+        print("You can vote!")
+    else:
+        print("You can't vote :(")
+
+def max_num(a,b,c):
+    if a > b and a > c:
+        print("a is the largest number with a value of " + str(a))
+    if b > a and b > c:
+        print("b is the largest number with a value of " + str(b))
+    if c > b and c > a:
+        print("c is the largest number with a value of " + str(c))
+
+def score_to_grade(score):
+    if score >= 90:
+        print("A")
+    elif score >= 80:
+        print("B")
+    elif score >= 70:
+        print("C")
+    elif score >= 60:
+        print("D")
+    else:
+        print("F")
+
+
+#Main
+vote_check()
+max_num(6,2,8)
+score_to_grade(82)
+
+#Boolean: True or False
+#Boolean Expression: 15 > 10 A statement that evaluates to true or false
+
+#Comparison Operators
+# > greater than
+# < less than
+# == equal to
+# >= greater than or equal to
+# <= less than or equal to
+# ! not equal to
+
+#Logical Operators
+# and - evaluates if both statements are true
+# or - evaluates to true if one statement is true
+# not - inverts the result of a boolean expression
